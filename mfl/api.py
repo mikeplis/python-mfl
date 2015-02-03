7d9d40fad7833121347562803ac3a6874424d16b
Mike Plis <mplis@novus.com>
2015-02-03 17:13:30 -0500

    implemented various import functions

--- mfl/api.py
+++ mfl/api.py
@@ -3,10 +3,16 @@ import urllib2
 import xml.etree.ElementTree as ET
 import time
 import datetime
+import sys
 
 def convert_to_timestamp(date):
     return time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
 
+# TODO: use this
+def concat(values):
+    return ','.join([str(s) for s in values])
+
+# TODO: figure out when it's okay to use [] as default instead of None
 class Api:
 
     _logged_in = False
@@ -27,6 +33,15 @@ class Api:
         resp = self.opener.open(url)
         return resp.read()
 
+    def _import(self, params, json=True):
+        if json:
+            params['JSON'] = 1
+        encoded_params = urllib.urlencode(params)
+        url = '{}?{}'.format(self.mfl_import_url, encoded_params)
+        resp = self.opener.open(url)
+        return resp.read()
+
+    # To login as commissioner, franchise_id = '0000'
     def login(self, league_id, franchise_id, password):
         params = urllib.urlencode({
             'L': league_id,
@@ -39,7 +54,7 @@ class Api:
         self.opener.addheaders.append(('Cookie', 'USER_ID={}'.format(user_id)))
         self.league_id = league_id
         self.franchise_id = franchise_id
-        self._logged_in = True
+        self._logged_in = True # may not be needed
 
     def players(self, players=None, since=None, details=False):
         params = {'TYPE': 'players'}
@@ -379,47 +394,154 @@ class Api:
     def salaries(self):
         pass
 
-    def accounting(self):
-        pass
+    def accounting(self, franchise_id, amount, description):
+        params = {
+            'TYPE': 'accounting',
+            'FRANCHISE_ID': franchise_id,
+            'AMOUNT': amount,
+            'DESCRIPTION': description,
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def franchise_score_adjustment(self):
-        pass
+    def franchise_score_adjustment(self, franchise_id, week, points, explanation):
+        params = {
+            'TYPE': 'franchiseScoreAdjustment',
+            'FRANCHISE_ID': franchise_id,
+            'WEEK': week,
+            'POINTS': points,
+            'EXPLANATION': explanation,
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def player_score_adjustment(self):
-        pass
+    def player_score_adjustment(self, player_id, week, points, explanation):
+        params = {
+            'TYPE': 'playerScoreAdjustment',
+            'PLAYER': player_id,
+            'WEEK': week,
+            'POINTS': points,
+            'EXPLANATION': explanation,
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def message_board(self):
-        pass
+    def message_board(self, body, thread=None, subject=None):
+        params = {
+            'TYPE': 'messageBoard',
+            'BODY': body,
+            'L': self.league_id
+        }
+        if thread is not None:
+            params['THREAD'] = thread
+        if subject is not None:
+            params['SUBJECT'] = subject
+        return self._import(params)
 
-    def lineup(self):
-        pass
+    def lineup(self, week, starters, comments='', tiebreakers=None, backups=None):
+        params = {
+            'TYPE': 'lineup',
+            'W': week,
+            'STARTERS': ','.join([str(pid) for pid in starters]),
+            'COMMENTS': comments,
+            'L': self.league_id
+        }
+        if tiebreakers is not None:
+            params['TIEBREAKERS'] = ','.join([str(pid) for pid in tiebreakers])
+        if backups is not None:
+            params['BACKUPS'] = ','.join([str(pid) for pid in backups])
+        return self._import(params)
 
-    def fcfs_waiver(self):
-        pass
+    def fcfs_waiver(self, add=None, drop=None):
+        params = {'TYPE': 'fcfsWaiver', 'L': self.league_id}
+        if add is not None:
+            params['ADD'] = ','.join([str(pid) for pid in add])
+        if drop is not None:
+            params['DROP'] = ','.join([str(pid) for pid in drop])
+        return self._import(params)
 
-    def waiver_request(self):
-        pass
+    def waiver_request(self, round_, picks):
+        params = {
+            'TYPE': 'waiverRequest',
+            'ROUND': round_,
+            'PICKS': ','.join([str(pid) for pid in picks]),
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def ir(self):
-        pass
+    def ir(self, activate=None, deactivate=None):
+        params = {'TYPE': 'IR', 'L': self.league_id}
+        if activate is not None:
+            params['ACTIVATE'] = concat(activate)
+        if deactivate is not None:
+            params['DEACTIVATE'] = concat(deactivate)
+        return self._import(params)
 
-    def taxi_squad(self):
-        pass
+    def taxi_squad(self, promote=None, demote=None):
+        params = {'TYPE': 'taxiSquad', 'L': self.league_id}
+        if promote is not None:
+            params['PROMOTE'] = concat(promote)
+        if demote is not None:
+            params['DEMOTE'] = concat(demote)
+        return self._import(params)
 
-    def my_watch_list(self):
-        pass
+    def my_watch_list(self, add=None, remote=None):
+        params = {'TYPE': 'myWatchList', 'L': self.league_id}
+        if add is not None:
+            params['ADD'] = ','.join([str(pid) for pid in add])
+        if drop is not None:
+            params['DROP'] = ','.join([str(pid) for pid in drop])
+        return self._import(params)
 
-    def poll_vote(self):
-        pass
+    def poll_vote(self, poll_id, answer_id):
+        params = {
+            'TYPE': 'pollVote',
+            'POLL_ID': poll_id,
+            'ANSWER_ID': answer_id,
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def trade_proposal(self):
-        pass
+    def trade_proposal(self,
+                       offered_to,
+                       will_give_up,
+                       will_receive,
+                       comments='',
+                       expires=None):
+        params = {
+            'TYPE': 'tradeProposal',
+            'OFFERED_TO': offered_to,
+            'WILL_GIVE_UP': concat(will_give_up),
+            'WILL_RECEIVE': concat(will_receive),
+            'COMMENTS': comments,
+            'L': self.league_id
+        }
+        if expires is not None:
+            params['EXPIRES'] = convert_to_timestamp(expires)
+        return self._import(params)
 
-    def trade_response(self):
-        pass
+    def trade_response(self,
+                       offered_to,
+                       will_give_up,
+                       offering_team,
+                       response):
+        params = {
+            'TYPE': 'tradeResponse',
+            'OFFERED_TO': offered_to,
+            'WILL_GIVE_UP': will_give_up,
+            'OFFERINGTEAM': offering_team,
+            'RESPONSE': response,
+            'L': self.league_id
+        }
+        return self._import(params)
 
-    def survivor_pool_pick(self):
-        pass
+    def survivor_pool_pick(self, pick):
+        params = {
+            'TYPE': 'survivorPoolPick',
+            'PICK': pick,
+            'L': self.league_id
+        }
+        return self._import(params)
 
     def pool_picks(self):
         pass
@@ -429,3 +551,8 @@ class Api:
 
     def email_message(self):
         pass
+
+if __name__ == '__main__':
+    api = Api(2014)
+    api.login('67486', '0001', sys.argv[1])
+    print(api.lineup(17, [9448, 9823]))
